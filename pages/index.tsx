8d800ee6e4a91b326242c05401157b0dac2bc298
Emmanuel Carvajal <emma2637@hotmail.com>
2021-10-13 16:07:30 -0600

    Adding company relation page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,14 @@
 import * as React from 'react'
 import { NextPage } from 'next'
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import Container from '@mui/material/Container';
+
 import Header from '../components/header/header.component'
 import Footer from '../components/footer/footer.component'
 import Slider from '../components/carousel/carousel.component'
-import Card from '../components/card/card.component'
+import CompanyRelation from '../components/companyRelation/companyRelation'
 import cardData from '../components/card/card.data'
 import { useState } from 'react'
 import iCardCar from '../components/card/iCardCar'
+
 
 
 const IndexPage: NextPage = () => {
@@ -23,32 +22,12 @@ const IndexPage: NextPage = () => {
 
 
 
-
   return (
     <div>
       <Header></Header>
       <div className="" >
         <Slider>loading!</Slider>
-
-
-        {/* <Container maxWidth="xl" fixed sx={{mb:3, backgroundColor:"#fc0;"}}> */}
-        {/* <Box sx={{ flexGrow: 1 }} > */}
-        <Grid container
-          spacing={{ xs: 1, md: 1, lg: 1, xl: 1 }}
-          direction="row"
-          justifyContent="center"
-          alignItems="center"
-          sx={{ mb: 3, backgroundColor: "#fc0;" }} >
-          {cards.map((card: iCardCar, index) => (
-            <Box key={index} >
-              <Grid item xs={4} sm={4} md={4} key={index} mb={20} sx={{ mt: 2 }}>
-                <Card {...card}></Card>
-              </Grid>
-            </Box>
-          ))}
-        </Grid>
-        {/* </Box> */}
-        {/* </Container> */}
+        <CompanyRelation data={cards}></CompanyRelation>
       </div>
       <div>
         <Footer></Footer>
@@ -59,6 +38,23 @@ const IndexPage: NextPage = () => {
 
 export default IndexPage
 
-function UseState(arg0: never[]): [any, any] {
-  throw new Error('Function not implemented.')
-}
+
+
+
+// <Grid container
+//           spacing={{ xs: 1, md: 1, lg: 1, xl: 1 }}
+//           direction="row"
+//           justifyContent="center"
+//           alignItems="center"
+//           sx={{ mb: 3, backgroundColor: "#fc0;" }} >
+
+//           <Box sx={{ flexGrow: 1 }} >
+
+//             {cards.map((card: iCardCar, index) => (
+//               <Grid item xs={4} sm={4} md={4} key={index} mb={20} sx={{ mt: 2 }}>
+//                   <Card {...card}></Card>
+//               </Grid>
+//             ))}
+//           </Box>
+
+//         </Grid>
