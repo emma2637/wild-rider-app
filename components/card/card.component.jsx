1c34ea94103cb94ccd4898c0cdd40420ceb08b15
Emmanuel Carvajal <emma2637@hotmail.com>
2021-10-13 14:22:03 -0600

    added Card Component

---
+++ components/card/card.component.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import Image from 'next/image'
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions'
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+
+
+// const ExpandMore = styled((props) => {
+//     const { expand, ...other } = props;
+//     return <IconButton {...other} />;
+//   })(({ theme, expand }) => ({
+//     transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+//     marginLeft: 'auto',
+//     transition: theme.transitions.create('transform', {
+//       duration: theme.transitions.duration.shortest,
+//     }),
+//   }))
+
+const CardExperience = (props) => {
+
+    console.log(props);
+  
+    return (
+        <Card sx={{ maxWidth: 350, maxHeight: 470, marginBottom:2, backgroundColor:"#f9f9f9", color:"#262f39 !important" }}>
+            <CardMedia
+                component="img"
+                height="194"
+                image={props.Image.path}
+                alt={props.Image.text}
+            />
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                    {props.Title.text}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                    {props.Description.text}
+                </Typography>
+            </CardContent>
+            <CardActions disableSpacing>
+                <Button size="small">
+                    {props.Actions.text}
+                </Button>
+            </CardActions>
+        </Card>
+
+    );
+}
+
+export default CardExperience;
---
+++ components/card/card.data.js
@@ -0,0 +1,81 @@
+const cardData = [{
+
+    Image: {
+        text: 'experience',
+        path: ''
+    },
+    Title: {
+        text: '25 Years of Costa Rica Experience',
+    },
+    Description: {
+        text: 'All of our customers receive in-depth training on the condition of the roads and the corresponding rules of conduct. Our customers leave the rental station well prepared.'
+    },
+    Actions: {
+        text: 'LEARN MORE',
+    }
+},
+{
+    Image: {
+        text: 'experience',
+        path: ''
+    },
+    Title: {
+        text: '25 Years of Costa Rica Experience',
+    },
+    Description: {
+        text: 'All of our customers receive in-depth training on the condition of the roads and the corresponding rules of conduct. Our customers leave the rental station well prepared.'
+    },
+    Actions: {
+        text: 'LEARN MORE',
+    }
+},
+{
+    Image: {
+        text: 'experience',
+        path: ''
+    },
+    Title: {
+        text: '25 Years of Costa Rica Experience',
+    },
+    Description: {
+        text: 'All of our customers receive in-depth training on the condition of the roads and the corresponding rules of conduct. Our customers leave the rental station well prepared.'
+    },
+    Actions: {
+        text: 'LEARN MORE',
+    }
+}
+,
+{
+    Image: {
+        text: 'experience',
+        path: ''
+    },
+    Title: {
+        text: '25 Years of Costa Rica Experience',
+    },
+    Description: {
+        text: 'All of our customers receive in-depth training on the condition of the roads and the corresponding rules of conduct. Our customers leave the rental station well prepared.'
+    },
+    Actions: {
+        text: 'LEARN MORE',
+    }
+}
+,
+{
+    Image: {
+        text: 'experience',
+        path: ''
+    },
+    Title: {
+        text: '25 Years of Costa Rica Experience',
+    },
+    Description: {
+        text: 'All of our customers receive in-depth training on the condition of the roads and the corresponding rules of conduct. Our customers leave the rental station well prepared.'
+    },
+    Actions: {
+        text: 'LEARN MORE',
+    }
+}
+]
+
+export default cardData;
