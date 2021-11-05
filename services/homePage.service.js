e784391622b095484d17eb8d82ca56a73376dbaa
Emmanuel Carvajal <emma2637@hotmail.com>
2021-11-05 15:40:24 -0600

    query updated

--- services/homePage.service.js
+++ services/homePage.service.js
@@ -82,6 +82,7 @@ export async function queryClient(language) {
                         url
                         width
                         height
+                        alternativeText
                       }
                       title
                       shortdescription
